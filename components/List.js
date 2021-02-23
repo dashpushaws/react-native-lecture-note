91884b19a28b804642c1d335375dca158ccc9db8
dashpushaws <dashpush.aws@gmail.com>
2021-02-23 09:50:22 +0900

    2.23 아침수정

--- components/List.js
+++ components/List.js
@@ -24,28 +24,29 @@ const List = ({ navigation }) => {
   const [list, setList] = useState([]);
   console.log('??')
   // [] < 컴포넌트가 App.js에 처음 마운트 됐을 때만 생성
-  // [] 생략시 재렌더링시마다 함수 재생성
   // memoizing function: []안의 객체 또는 변수가 생성되거나 바뀔때 함수가 생성
   // 컴포넌트가 새로 마운트 됐을때 데이터를 호출시킬 것이기 때문에
 
   // useCallback(function(), [xxx]): 특정 조건에서 생성되는 함수를 만들 때 쓰는 hook
   // useCallback(function(), []): 컴포넌트가 처음 마운트 될 때, 함수 생성
   // useCallback(function(), [data]): data(변수, 객체)가 생성 혹은 수정될 때, 함수 생성
+  // useCallback(function()): 컴포넌트 렌더링 될때마다 계속 함수 생성
   const getList = useCallback(async () => {
     const result = await api.list();
     console.log('2-useCallback')
-    // console.log(result.data);
     // state를 갱신 -> 재렌더링
     setList(result.data);
   }, [])
   console.log('???')
 
-  // componentDidMount(event hook)-컴포넌트가 마운트 됐을때 함수 정의
+ 
 
   // useEffect(function(), [xxx]): 특정 조건에서 실행하는 함수를 만들 때 쓰는 hook
   // useEffect(function(), []): 컴포넌트가 처음 마운트 될 때, 함수 실행
   // useEffect(function(), [data]): data(변수, 객체)가 생성 혹은 수정될 때, 함수 실행
-   
+  // useEffect(function()): 컴포넌트가 렌더링 될때마다 계속 함수 호출
+  // componentDidMount(event hook)-컴포넌트가 마운트 됐을때 함수 실행
+
   // useEffect(() => {
   //   console.log('1-useEffect')
   //   getList();
